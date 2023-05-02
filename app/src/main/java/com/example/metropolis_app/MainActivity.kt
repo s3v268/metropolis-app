b852e84ae836fc21334bd09b939c3b0e2c1a9a08
sve268 <sve2680@gmail.com>
2023-05-02 20:22:50 +0200

    Added Viewbinding and intent function

--- app/src/main/java/com/example/metropolis_app/MainActivity.kt
+++ app/src/main/java/com/example/metropolis_app/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.metropolis_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.metropolis_app.databinding.ActivityMainBinding
+import com.example.metropolis_app.espacios.EspaciosActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnEspacios.setOnClickListener{
+            goTo(Intent(this, EspaciosActivity ::class.java))
+        }
+    }
+
+    private fun goTo(intent: Intent){
+        startActivity(intent)
     }
 }
