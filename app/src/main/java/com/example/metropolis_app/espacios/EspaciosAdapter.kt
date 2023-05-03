e13cb78f1459285081ec2b432f242a9a377fee9f
Abdellah Ech Chana <abdellahechchana@gmail.com>
2023-05-03 20:32:27 +0200

    Creando recycler view

--- app/src/main/java/com/example/metropolis_app/espacios/EspaciosAdapter.kt
+++ app/src/main/java/com/example/metropolis_app/espacios/EspaciosAdapter.kt
@@ -1,4 +1,38 @@
 package com.example.metropolis_app.espacios
 
-class EspaciosAdapter {
-}
+import android.content.Context
+import android.graphics.Movie
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.metropolis_app.R
+
+/*class EspaciosAdapter(
+    //var espacios: List</*Espacio*/> = emptyList(),
+    private val mContext: Context,
+    private val onItemClicked: (/*Espacio*/) -> Unit
+) : RecyclerView.Adapter<EspaciosViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EspaciosViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return EspaciosViewHolder(
+            layoutInflater.inflate(R.layout.item_espacios, parent, false),
+            mContext
+        )
+    }
+
+    override fun onBindViewHolder(viewHolder: EspaciosViewHolder, position: Int) {
+
+        val espacio = espacios[position]
+        viewHolder.bind(/*espacio*/)
+
+
+        //para capturar el click en el elemento
+        //viewHolder.itemView.setOnClickListener { onItemClicked(espacio) }
+
+    }
+
+   // override fun getItemCount(): Int = espacio.size
+
+
+}*/
---
+++ app/src/main/java/com/example/metropolis_app/espacios/EspaciosViewHolder.kt
@@ -0,0 +1,15 @@
+package com.example.metropolis_app.espacios
+
+import android.content.Context
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class EspaciosViewHolder(val view: View, val mContext: Context) : RecyclerView.ViewHolder(view) {
+
+    //private val binding = NewMovieItemBinding.bind(view)
+
+    fun bind( /*espacio: Espacio*/ ) {
+
+
+    }
+}
