ccd97612f1859d933dd0910d114736396fb8a327
sve268 <sve2680@gmail.com>
2023-05-03 20:20:09 +0200

    Added intents, and form(incomplete)

---
+++ app/src/main/java/com/example/metropolis_app/FormularioActivity.kt
@@ -0,0 +1,32 @@
+package com.example.metropolis_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import com.example.metropolis_app.databinding.ActivityFormularioBinding
+import com.example.metropolis_app.databinding.ActivityMainBinding
+
+class FormularioActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityFormularioBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFormularioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.attendeesSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, fromUser: Boolean) {
+                val numAttendees = progress
+                //numAttendeesTextView.text = numAttendees.toString()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+}
