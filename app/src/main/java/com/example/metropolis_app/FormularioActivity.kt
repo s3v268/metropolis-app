3ef0085f8c2e30269408b1096159db5d64049f4a
sve268 <sve2680@gmail.com>
2023-05-09 15:48:22 +0200

    Implementado checkbox, Spinner, Datepicker with custom style

--- app/src/main/java/com/example/metropolis_app/FormularioActivity.kt
+++ app/src/main/java/com/example/metropolis_app/FormularioActivity.kt
@@ -1,14 +1,18 @@
 package com.example.metropolis_app
 
-import android.R
+import com.example.metropolis_app.R
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.ArrayAdapter
+import android.widget.CheckBox
+import android.widget.DatePicker
+import android.widget.LinearLayout
 import android.widget.SeekBar
+import android.widget.TextView
 import com.example.metropolis_app.databinding.ActivityFormularioBinding
-import com.example.metropolis_app.databinding.ActivityMainBinding
 import java.util.Calendar
 
 class FormularioActivity : AppCompatActivity() {
@@ -18,14 +22,29 @@ class FormularioActivity : AppCompatActivity() {
         binding = ActivityFormularioBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //spinner
+        configureView()
+
+    }
+
+    private fun configureView() {
+        configureSpinner()
+        configureSeekBar()
+        configureDatePickerDialog()
+    }
+
+
+    private fun configureCheckbox(){
+
+    }
+    private fun configureSpinner() {
         val spinner = binding.spinner
-        val items = arrayOf("Sala A", "Sala B", "Sala C", "Padock", "Sala Conf. 1")
-        val adapter = ArrayAdapter(this, R.layout.simple_spinner_item, items)
+        val items = arrayOf("--", "Sala A", "Sala B", "Sala C", "Padock", "Sala Conf. 1")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
+    }
 
-        //seekbar
+    private fun configureSeekBar() {
         binding.attendeesSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, progress: Int, fromUser: Boolean) {
                 val thumbRect = binding.attendeesSeekBar.thumb.bounds
@@ -40,34 +59,49 @@ class FormularioActivity : AppCompatActivity() {
 
             override fun onStopTrackingTouch(p0: SeekBar?) {
             }
-
         })
-
-        //datepicker
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-        binding.tvFechainicio.setOnClickListener {
-            val datePickerDialog = DatePickerDialog(this, { _, year, month, dayOfMonth ->
-                val date = "$dayOfMonth/${month + 1}/$year"
-                binding.tvFechainicio.text = date
-            }, year, month, day)
-            datePickerDialog.show()
-        }
-        /*SPINNER
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-        binding.tvFechainicio.setOnClickListener {
-            val datePickerDialog = DatePickerDialog(this, android.R.style.Theme_Holo_Light_Dialog_NoActionBar, { _, year, month, dayOfMonth ->
-                val date = "$dayOfMonth/${month + 1}/$year"
-                binding.tvFechainicio.text = date
-            }, year, month, day)
-            datePickerDialog.datePicker.calendarViewShown = false
-            datePickerDialog.show()
-        }
-        * */
     }
-}
+
+    fun configureDatePickerDialog() {
+        binding.tvFechainicio.setOnClickListener{
+            createDatePickerDialog(it as TextView)
+        }
+        binding.tvFechafinal.setOnClickListener{
+            createDatePickerDialog(it as TextView)
+        }
+    }
+
+    private fun createDatePickerDialog( dateTv: TextView){
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        //para quitar el spinner mode quitar el stylo de los parametros i las 3 lineas de abajo
+        val datePickerDialog = DatePickerDialog(this, R.style.CustomDatePickerDialogTheme, { _, year, month, dayOfMonth ->
+            val date = "$dayOfMonth/${month + 1}/$year"
+            dateTv.text = date
+        }, year, month, day)
+
+        datePickerDialog.datePicker.calendarViewShown = false
+
+        datePickerDialog.show()
+    }
+
+    fun onBusPassCheckboxClicked(view: View) {
+        val busPassCheckbox = binding.busPassCheckbox
+        val busPassContainer = binding.busPassContainer
+        if (busPassCheckbox.isChecked) {
+            // Checkbox is checked, show the associated views
+            busPassContainer.visibility = View.VISIBLE
+            // Add animation to show the container
+            val animation = AnimationUtils.loadAnimation(this, R.anim.slide_in_up)
+            busPassContainer.startAnimation(animation)
+        } else {
+            // Checkbox is unchecked, hide the associated views
+            busPassContainer.visibility = View.GONE
+        }
+        }
+    }
+
+
